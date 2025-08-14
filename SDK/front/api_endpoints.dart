310adba55fa950547324d38b39b264c2cbe2bb90
XayUp <lucasjrcostadacosta@gmail.com>
2025-08-14 11:00:11 -0400

    Uma pasta SDK para facilitar o desenvolvimento do front ao utilizar as APIs

---
+++ SDK/front/api_endpoints.dart
@@ -0,0 +1,7 @@
+class APIEndpoints {
+  static const String baseUrl = ''; // Altere para o IP do seu servidor, se necessário
+  static const String login = '$baseUrl/login';
+  static const String products = '$baseUrl/products';
+  static const String register = '$baseUrl/register';
+  static const String adminUpload = '$baseUrl/admin/upload';
+}
---
+++ routes/register/index.dart
@@ -0,0 +1,6 @@
+import 'package:dart_frog/dart_frog.dart';
+
+Response onRequest(RequestContext context) {
+  //TODO: Implementar o processamento de dados de registros recebidos
+  return Response(body: 'Hello, World!');
+}
