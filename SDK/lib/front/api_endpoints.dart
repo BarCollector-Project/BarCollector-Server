1fa01efd9a15ccc64b355ed583a1e0bc6d7ea0a3
XayUp <lucasjrcostadacosta@gmail.com>
2025-09-06 16:02:41 -0400

    Adicionado o endpoint para consulta de tabela de sistema

--- SDK/lib/front/api_endpoints.dart
+++ SDK/lib/front/api_endpoints.dart
@@ -1,3 +1,5 @@
+import 'package:barcollector_sdk/types/register/system_tables.dart';
+
 class APIEndpoints {
   static const String baseUrl = ''; // Altere para o IP do seu servidor, se necessário
   static const String login = '$baseUrl/login';
@@ -5,4 +7,9 @@ class APIEndpoints {
   static const String register = '$baseUrl/register';
   static const String adminUpload = '$baseUrl/admin/upload';
   static const String validity = '$baseUrl/register/validity';
+
+  /// Esta rota é a rota principal, onde a rota seguinte deve ser o nome da tablea, ou seja,
+  /// .../system_table/<NOME_TABELA>, por exemplo .../system_table/st_motivocoleta.
+  /// Estes nomes podem ser obtidos em [SystemTables].
+  static const String systemTables = '$baseUrl/system_table';
 }
